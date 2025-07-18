3ff9530726782afba0bd84d656f180c15dd3e6a4
Marteedbug9 <martyveson2@email.com>
2025-07-18 19:37:21 -0400

    Initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,20 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head'; // <--- Ajouté
 import { appWithTranslation } from 'next-i18next';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/icon.png" type="image/png" />
+        <link rel="icon" href="/icon.png" sizes="32x32" type="image/png" />
+        <link rel="icon" href="/icon.png" sizes="192x192" type="image/png" />
+        <title>ARDH | Assistance for Refugees</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default appWithTranslation(MyApp);
