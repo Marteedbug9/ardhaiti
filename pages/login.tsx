df4f1b8ac503d00f5fef8e6262eb3e4fca806304
Marteedbug9 <martyveson2@email.com>
2025-07-22 15:03:38 -0400

    Initial commit

--- pages/login.tsx
+++ pages/login.tsx
@@ -29,7 +29,7 @@ export default function Login() {
       return;
     }
     try {
-      const res = await fetch(`${API_URL}/auth/login`, {
+      const res = await fetch(`${API_URL}/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
